a6d28d490c793ef4ef956b4d16b29d24e051e3a3
Ian C <fortytw2@gmail.com>
2015-07-03 18:17:33 -0500

    pgsql: fix tab/space issues in queries

--- datastore/pgsql/queries/board.go
+++ datastore/pgsql/queries/board.go
@@ -3,27 +3,27 @@ package queries
 func addBoardQueries() {
 	// inserts a new board
 	queries["insert_board"] = `
-  INSERT INTO boards (name, creator, mods, summary)
+    INSERT INTO boards (name, creator, mods, summary)
          VALUES (:name, :creator, :mods, :summary)`
 
 	queries["update_board"] = `
-	UPDATE boards SET name = :name,
+    UPDATE boards SET name = :name,
                   creator = :creator,
                   mods = :mods,
                   summary = :summary,
-									deleted = :deleted,
-									approved = :approved
+                  deleted = :deleted,
+                  approved = :approved
                   WHERE id = :id`
 
 	queries["get_board_by_name"] = `
-	SELECT * FROM boards WHERE name = $1;`
+    SELECT * FROM boards WHERE name = $1;`
 
 	queries["get_board_by_id"] = `
-	SELECT * FROM boards WHERE id = $1;`
+    SELECT * FROM boards WHERE id = $1;`
 
 	queries["delete_board_by_id"] = `
-	DELETE FROM boards WHERE id = $1;`
+    DELETE FROM boards WHERE id = $1;`
 
 	queries["get_all_boards"] = `
-	SELECT * FROM boards LIMIT 50 OFFSET $1;`
+    SELECT * FROM boards LIMIT 50 OFFSET $1;`
 }
--- datastore/pgsql/queries/user.go
+++ datastore/pgsql/queries/user.go
@@ -3,12 +3,12 @@ package queries
 func addUserQueries() {
 	// inserts a new user
 	queries["insert_user"] = `
-  INSERT INTO users (username, email, passwordhash, confirmed)
+    INSERT INTO users (username, email, passwordhash, confirmed)
          VALUES (:username, :email, :passwordhash, :confirmed)`
 
 	// update_user updates the user given by id
 	queries["update_user"] = `
-  UPDATE users SET username = :username,
+    UPDATE users SET username = :username,
                   passwordhash = :passwordhash,
                   email = :email,
                   confirmed = :confirmed
@@ -16,9 +16,9 @@ func addUserQueries() {
 
 	// get a user by id
 	queries["get_user_by_id"] = `
-  SELECT * FROM users WHERE id = $1;`
+    SELECT * FROM users WHERE id = $1;`
 
 	// get a user by username
 	queries["get_user_by_username"] = `
-  SELECT * FROM users WHERE username = $1;`
+    SELECT * FROM users WHERE username = $1;`
 }
